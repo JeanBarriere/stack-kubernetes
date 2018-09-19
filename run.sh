bf2cb2eba9cc0e8d3bb138a4be422c6d44f955f9
Jude Pereira <contact@judepereira.com>
2018-09-19 12:44:09 +0530

    Add Helm + Nginx + secrets bootstrap.

--- run.sh
+++ run.sh
@@ -7,10 +7,18 @@ check_kubectl() {
     exit 1
   fi
   echo "Current kubectl context: $(kubectl config current-context)"
-  read -p "Continue (y/n)?" CONT
+  read -p "Continue (y/n)? " CONT
   if [ "$CONT" != "y" ]; then
     echo "EXITING - NO CHANGES MADE"
     exit
+  fi
+}
+
+check_helm() {
+  if ! hash helm; then
+    echo "ERROR: helm not found"
+    echo "Please install the Helm client only from here https://docs.helm.sh/using_helm/#from-script"
+    exit 1
   fi
 }
 
@@ -76,12 +84,52 @@ istio_install() {
   echo "Service Graph: $ kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=servicegraph -o jsonpath='{.items[0].metadata.name}') 8088:8088"
 }
 
+asyncy_install_secrets() {
+  read -p "Sentry DSN: " SENTRY_DSN
+  read -p "Postgres Host: " PG_HOST
+  read -p "Postgres DB Name: " PG_DB_NAME
+  read -p "Postgres DB Username: " PG_USERNAME
+  read -p "Postgres DB Password: " -s PG_PASSWORD
+  echo
+  CONNECTION_STRING="options=--search_path=app_public,app_hidden,app_private,public dbname=$PG_DB_NAME host=$PG_HOST user=$PG_USERNAME password=$PG_PASSWORD"
+  CONNECTION_STRING_URI="postgres://$PG_USERNAME:$PG_PASSWORD@$PG_HOST/$PG_DB_NAME"
+  PG_HOST= PG_DB_NAME= PG_USERNAME= PG_PASSWORD=
+
+  kubectl create secret generic database-url --from-literal=asyncy-authenticator="$CONNECTION_STRING" --from-literal=postgres="$CONNECTION_STRING" --from-literal=postgres_conn_string="$CONNECTION_STRING_URI"
+  CONNECTION_STRING= CONNECTION_STRING_URI=
+
+  kubectl create secret generic sentry --from-literal=sentry_dsn=$SENTRY_DSN
+  SENTRY_DSN=
+
+  read -p "Full path to privkey.pem (for api.asyncy.com): " PRIV_KEY
+  read -p "Full path to fullchain.pem (for api.asyncy.com): " FULLCHAIN
+  kubectl create secret tls asyncy.com --key $PRIV_KEY --cert $FULLCHAIN
+}
+
 asyncy_install() {
   check_kubectl
   check_skaffold
+  check_helm
+
+  echo "Installing Helm and Nginx"
+  # create RBAC role and cluster binding for Tiller - the Helm server
+  kubectl create serviceaccount --namespace kube-system tiller
+  kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+  kubectl create -f kubernetes-pre-init/tiller-cluster-role-binding.yaml
+  helm init --service-account tiller --upgrade
+  while ! kubectl get pod -n kube-system | grep tiller-deploy | grep Running &> /dev/null; do
+    echo "Waiting for the tiller-deploy pod to be ready..."
+    sleep 1
+  done
+  helm install --name nginx-ingress stable/nginx-ingress --set rbac.create=true
+
   echo "Installing Asyncy..."
   # creating the asyncy namespace
   kubectl create namespace asyncy-system
+
+  # create the secrets required
+  asyncy_install_secrets
+
 
   # enabling istio sidecar injection
   kubectl label namespace asyncy-system istio-injection=enabled > /dev/null
@@ -112,6 +160,7 @@ usage() {
   echo "Options:"
   echo "istio-install"
   echo "asyncy-install"
+  echo "asyncy-install-secrets"
   echo "asyncy-dev"
   echo "asyncy-update"
 }
@@ -128,6 +177,9 @@ main() {
         ;;
       asyncy-install)
         asyncy_install
+        ;;
+      asyncy-install-secrets)
+        asyncy_install_secrets
         ;;
       asyncy-dev)
         asyncy_dev
